f6086024a4d94c31d5526b0e9d7a6a39c6787fe0
Juhan Brayan <juhan.andrade@aluno.ce.gov.br>
2026-09-24 11:04:33 -0300

    Implement purchase summary calculation
    Calculate total price and generate a summary for a purchase.

--- 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
+++ 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
@@ -1,5 +1,31 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
+const cliente = "Mariana Silva"
+const cidade = "Fortaleza"
+const produto = "Notebook Pro"
+const categoria = "Notebook"
+let preco = 3500
+let quantidade = 2
+let descontoPercentual = 10
+let valorPago = 7000
 
+const subtotal = preco * quantidade
+const valorDesconto = subtotal * descontoPercentual
+const valorFinal = subtotal - valorDesconto
+const troco = valorPago - valorFinal
+
+const resumo = `
+cliente= ${cliente}
+cidade= ${cidade}
+  produto= ${produto}
+  categoria= ${categoria}
+  preco= ${preco}
+  quantidade= ${quantidade}
+  descontoPercentual= ${descontoPercentual}
+  valorPago= ${valorpago}
+  subtotal= ${subtotal}
+  valorDesconto= ${valorDesconto}
+  valoFinal= ${valorFinal}
+  troco= ${troco}
 
 
 
